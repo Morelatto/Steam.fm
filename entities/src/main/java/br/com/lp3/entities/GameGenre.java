d7bc55c7495279b6b738d62ad300dc003c2adbba
Pedro Morelatto <pmichilis@uolinc.com>
2018-11-26 12:49:22 -0200

    Updated GameGenre entity.

--- entities/src/main/java/br/com/lp3/entities/GameGenre.java
+++ entities/src/main/java/br/com/lp3/entities/GameGenre.java
@@ -9,12 +9,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name="GAME_GENRE")
 public class GameGenre implements Serializable {
 
